a2578c36d279ac88f1044022ac457ffcd7b8b641
Creighton Ethan Cook <creigh3@clemson.edu>
2017-11-10 04:36:03 -0500

    Working on calendar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,10 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
 
         <!-- Styles -->
         <style>
@@ -70,11 +74,13 @@
         </style>
     </head>
     <body>
+      @section ('content')
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
                     <img src="../img/MVF-farms-logo.png" alt="Mill Village Farms">
                 </div>
+
                 <div class="container">
                   <div class="row">
                       <div class="col-md-8 col-md-offset-2">
@@ -114,33 +120,70 @@
                                         </div>
 
                                         <div class="form-group">
-                                            <!--<div class="col-md-6 col-md-offset-4"> -->
+                                        <!--  <div class="col-md-6 col-md-offset-4"> -->
                                                 <div class="checkbox">
                                                     <label>
                                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
                                                     </label>
                                                 </div>
-                                            <!-- </div> -->
+                                        <!--    </div> -->
                                         </div>
 
                                         <div class="form-group">
-                                            <!-- <div class="col-md-8 col-md-offset-4"> -->
+                                          <!--   <div class="col-md-8 col-md-offset-4"> -->
                                                 <button type="submit" class="btn btn-primary">
                                                     Login
                                                 </button>
 
-                                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                <button
+                                                     type="button"
+                                                     class="btn btn-link"
+                                                     data-toggle="modal"
+                                                     data-target="#resetModal">
                                                     Forgot Your Password?
-                                                </a>
-                                          <!--   </div> -->
-                                        </div>
+                                                  </button>
+
+                                                  <div class="modal fade" id="resetModal"
+                                                       tabindex="-1" role="dialog"
+                                                       aria-labelledby="resetModalLabel">
+                                                    <div class="modal-dialog" role="document">
+                                                      <div class="modal-content">
+                                                        <div class="modal-header">
+                                                          <button type="button" class="close"
+                                                            data-dismiss="modal"
+                                                            aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span></button>
+                                                          <h4 class="modal-title"
+                                                          id="favoritesModalLabel">Forgot Your Password</h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                          <p>
+                                                          Please confirm you would like to
+                                                          <b><span id="fav-title">contact the adminstrator</span></b>
+                                                          about resetting your password.
+                                                          </p>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                          <button type="button"
+                                                             class="btn btn-default"
+                                                             data-dismiss="modal">Close</button>
+                                                          <span class="pull-right">
+                                                            <button type="button" class="btn btn-primary">
+                                                              Reset My Password
+                                                            </button>
+                                                          </span>
+                                                        </div>
+                                                      </div>
+                                                    </div>
+                                                  </div>
+                                    <!--    </div> -->
                                     </form>
                                   </div>
                                 </div>
                         </div>
                     </div>
                 </div>
-
+@endsection
 
             </div>
         </div>
