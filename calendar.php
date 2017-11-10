e0a0e8d0b9d16e7d7d76c3072e262c3c2113bfe7
Nicholas Paul <ntpaul@clemson.edu>
2017-11-10 00:06:04 -0500

    i did

--- calendar.php
+++ calendar.php
@@ -1,55 +1,235 @@
 ﻿<!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-< href="stylesheet"
-<href="java"
+
 <head>
+	<title>Mill Village Farms </title>
     <meta charset="utf-8" />
-    <title></title>
-    <style type>
-
-		.mainContainer{
-			min-width: 1150px;
-		}
-
-        tr{
-        border: 1px solid black;
-		width 50px;
-		height 50px;
-
-    }
-    
-    th{
-    border: 1px solid black;
-	width 50px;
-	height 50px;
-}
-
-    td{
-    border: 1px solid black;
-	width 50px;
-	height 50px;
-}
-        
-
-
-    </style>
+	<link rel="stylesheet" href="StyleSheet1.css">
+	<link rel="stylesheet" href="bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css">
 </head>
 <body>
 	<!-- <?php include '.php';?> -->
-    <div class="mainContainer">
 
+	
+    <div class="container">
+		<!-- use css grids instead of divs to auto format on mobile devices-->
+		<div class="buttons">
+			<button id="create"> Create </button>
+			<button id="make"> Make </button>
+			<button id="students"> Students </button>
+		</div>
+
+
+
+		<h2 id="month">June</h2>
+		<div class="row">
+			<div class="col" id="row1">
+				S
+			</div>
+				
+			<div class="col" id="row1">
+				M
+			</div>
+				
+			<div class="col" id="row1">
+				T
+			</div>
+
+			<div class="col" id="row1">
+				W
+			</div>
+
+			<div class="col" id="row1">
+				T
+			</div>
+
+			<div class="col" id="row1">
+				F
+			</div>
+
+			<div class="col" id="row1">
+				S
+			</div>
+		</div>
+
+
+
+		<div class="row">
+			<div class="col" id="row2">
+				<span>Row 2</span>
+			</div>
+
+			<div class="col" id="row2">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row2">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row2">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row2">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row2">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row2">
+				<span> </span>
+			</div>
+		</div>
+
+
+		<div class="row">
+			<div class="col" id="row3">
+				<span> Row 3 </span>
+			</div>
+
+			<div class="col" id="row3">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row3">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row3">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row3">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row3">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row3">
+				<span> </span>
+			</div>
+		</div>
+
+
+		<div class="row">
+			<div class="col" id="row4">
+				<span> Row 4 </span>
+			</div>
+
+			<div class="col" id="row4">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row4">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row4">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row4">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row4">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row4">
+				<span> </span>
+			</div>
+		</div>
+
+
+
+		<div class="row">
+			<div class="col" id="row5">
+				<span> Row 5 </span>
+			</div>
+
+			<div class="col" id="row5">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row5">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row5">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row5">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row5">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row5">
+				<span> </span>
+			</div>
+		</div>
+
+
+		<div class="row">
+			<div class="col" id="row6">
+				<span> Row 6 </span>
+			</div>
+
+			<div class="col" id="row6">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row6">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row6">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row6">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row6">
+				<span> </span>
+			</div>
+
+			<div class="col" id="row6">
+				<span> </span>
+			</div>
+		</div>
+
+	</div><!-- container -->
+
+
+
+
+
+
+	<!--
     <table>
-		<tr id="month">
-			<h2>June</h3>
+		<tr>
+			<h2 id="month">June</h2>
 		</tr>
+
        <tr>
-        <th>S</th>
-        <th>M</th>
-        <th>T</th>
-        <th>W</th>
-        <th>T</th>
-        <th>F</th>
-        <th>S</th>
+        <th class="days">S</th>
+        <th class="days">M</th>
+        <th clas="days">T</th>
+        <th class="days">W</th>
+        <th class="days">T</th>
+        <th class="days">F</th>
+        <th class="days">S</th>
        </tr>
 
 
@@ -108,7 +288,7 @@
         </tr>
 
     </table>
+	-->
 
-    </div>
 </body>
 </html>
