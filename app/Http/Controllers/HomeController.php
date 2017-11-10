9021af060291a6ec8e879b913166734e1f852e0d
Keith Hammock <basky450@gmail.com>
2017-11-10 09:07:25 -0500

    Lots

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,23 +6,35 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
+
+//should include a function to automatically redirect users to the login page, but somehow that already exists 
+/*
+	public function getIndex() {
+		return Redirect::to("User");
+	}
+*/
+
+
+
+/* not sure what any of this does 
+    
      * Create a new controller instance.
      *
      * @return void
-     */
+     
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
+    
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
-     */
+     
     public function index()
     {
         return view('home');
     }
+*/
 }
