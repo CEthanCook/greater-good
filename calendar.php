a6bcd970cc836d43d27c8845ad83d6f3d070e3b6
Nicholas Paul <ntpaul@clemson.edu>
2017-11-09 22:26:06 -0500

    added bootsrap

---
+++ calendar.php
@@ -0,0 +1,114 @@
+﻿<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+< href="stylesheet"
+<href="java"
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <style type>
+
+		.mainContainer{
+			min-width: 1150px;
+		}
+
+        tr{
+        border: 1px solid black;
+		width 50px;
+		height 50px;
+
+    }
+    
+    th{
+    border: 1px solid black;
+	width 50px;
+	height 50px;
+}
+
+    td{
+    border: 1px solid black;
+	width 50px;
+	height 50px;
+}
+        
+
+
+    </style>
+</head>
+<body>
+	<!-- <?php include '.php';?> -->
+    <div class="mainContainer">
+
+    <table>
+		<tr id="month">
+			<h2>June</h3>
+		</tr>
+       <tr>
+        <th>S</th>
+        <th>M</th>
+        <th>T</th>
+        <th>W</th>
+        <th>T</th>
+        <th>F</th>
+        <th>S</th>
+       </tr>
+
+
+        <tr id="firstRow">
+            <td></td>   
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+
+
+        <tr id="secondRow">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+        <tr id="thirdRow">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+
+        <tr id="fourthRow">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+
+        <tr id="fifthRow">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+    </table>
+
+    </div>
+</body>
+</html>
