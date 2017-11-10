000bc0cc6c571693f257f206387465d25e2c89d1
Keith Hammock <basky450@gmail.com>
2017-11-10 00:42:29 -0500

    hi

--- calendarFunctions.php
+++ calendarFunctions.php
@@ -8,20 +8,36 @@ function configureCalAr($month, $year){
 	$dayCount = 0;
 	$dayArray[7][5] = 0;
 	$totalDays=cal_days_in_month(CAL_GREGORIAN,$month,$year);
-	echo $totalDays;
+	//echo $totalDays;
 	$jd=gregoriantojd(1,$month,$year);  
-	echo $jd;
+	//echo $jd;
 	$dayOfWeek = jddayofweek($jd,1);
-	echo $dayOfWeek;
-	<!--decision statement for determining correct starting point -->
-	if(strcmp($dayOfWeek, "Sunday")) $dayArray[0][0] = 1;
-	if(strcmp($dayOfWeek, "Monday")) $dayArray[1][0] = 1;
-	if(strcmp($dayOfWeek, "Tuesday")) $dayArray[2][0] = 1;
-	if(strcmp($dayOfWeek, "Wednesday")) $dayArray[3][0] = 1;
-	if(strcmp($dayOfWeek, "Thursday")) $dayArray[4][0] = 1;
-	if(strcmp($dayOfWeek, "Friday")) $dayArray[5][0] = 1;
-	if(strcmp($dayOfWeek, "Saturday")) $dayArray[6][0] = 1;
-	<!--initialize the rest of the dayArray -->
+	//echo $dayOfWeek;
+
+	//decision statement for determining correct starting point 
+	if(!strcmp($dayOfWeek, "Sunday")){
+		$dayArray[0][0] = 1;
+	}
+	else if(!strcmp($dayOfWeek, "Monday")){
+		$dayArray[1][0] = 1;
+	}
+	else if(!strcmp($dayOfWeek, "Tuesday")){ 
+		$dayArray[2][0] = 1;
+	}
+	else if(!strcmp($dayOfWeek, "Wednesday")){
+		//echo "Great";
+		$dayArray[3][0] = 1;
+	}
+	else if(!strcmp($dayOfWeek, "Thursday")){
+		$dayArray[4][0] = 1;
+	}
+	else if(!strcmp($dayOfWeek, "Friday")){
+		$dayArray[5][0] = 1;
+	}
+	else if(!strcmp($dayOfWeek, "Saturday")){
+		$dayArray[6][0] = 1;
+	}
+	//initialize the rest of the dayArray 
 	for($i = 0; $i < 7; $i++){
 		for($j = 0; $j < 5; $j++){
 			if($dayArray[$i][$j] == 1){
@@ -30,7 +46,8 @@ function configureCalAr($month, $year){
 			}
 			if($dayCount <= $totalDays && $foundDayOne){
 				$dayArray[$i][$j] = $dayCount;
-				if($dayCount == $totalDays && $foundDayOne){
+				if($dayCount == $totalDays){
+					echo dayArray[3][0];
 					return $dayArray;
 				}
 			}
