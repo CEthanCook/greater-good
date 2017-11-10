b4adb062b3c57d37dc0837ee6280592fb6d7e39d
Keith Hammock <basky450@gmail.com>
2017-11-10 00:21:11 -0500

    Renamed filed

--- calendarFunctions.php
+++ calendarFunctions.php
@@ -1,15 +1,18 @@
-﻿function configureCalAr($month, $year){
+﻿<!DOCTYPE html>
+<html>
+
+<body>
+<?php
+function configureCalAr($month, $year){
 	$foundDayOne = 0;
 	$dayCount = 0;
 	$dayArray[7][5] = 0;
 	$totalDays=cal_days_in_month(CAL_GREGORIAN,$month,$year);
 	echo $totalDays;
-	$jd=gregoriantojd(1,$month,$year); 
+	$jd=gregoriantojd(1,$month,$year);  
 	echo $jd;
 	$dayOfWeek = jddayofweek($jd,1);
 	echo $dayOfWeek;
-
-
 	<!--decision statement for determining correct starting point -->
 	if(strcmp($dayOfWeek, "Sunday")) $dayArray[0][0] = 1;
 	if(strcmp($dayOfWeek, "Monday")) $dayArray[1][0] = 1;
@@ -18,7 +21,6 @@
 	if(strcmp($dayOfWeek, "Thursday")) $dayArray[4][0] = 1;
 	if(strcmp($dayOfWeek, "Friday")) $dayArray[5][0] = 1;
 	if(strcmp($dayOfWeek, "Saturday")) $dayArray[6][0] = 1;
-
 	<!--initialize the rest of the dayArray -->
 	for($i = 0; $i < 7; $i++){
 		for($j = 0; $j < 5; $j++){
@@ -35,6 +37,9 @@
 		}
 	}
 }
-
 $dayArray[7][5] = configureCalAr(11, 2017);
 echo $dayArray[3][0];
+?>
+<span> <?php echo $dayArray[3][0]; ?></span>
+</body>
+</html>
