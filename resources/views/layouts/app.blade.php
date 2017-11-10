51c4efeef4ffd7ff3c323b0f8114714890ff36ef
Creighton Ethan Cook <creigh3@clemson.edu>
2017-11-10 09:06:18 -0500

    Changed dependencies to use https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Mill Village Farms') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Mill Village Farms') }}
+                        {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
 
@@ -64,12 +64,11 @@
                                     </li>
                                 </ul>
                             </li>
-                        @endif
+                          @endif
                     </ul>
                 </div>
             </div>
         </nav>
-
         @yield('content')
     </div>
 
