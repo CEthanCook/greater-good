13157061592bb0d2430c0e5c8c9ae82ceb8881cb
Zach Wyman <zachwyman@zachs-mbp.attlocal.net>
2017-11-10 02:09:33 -0500

    Add user to database page

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/user', 'UserController@index')->name('user');
+Route::get('/user', function () {
+  return view('user');
+};
