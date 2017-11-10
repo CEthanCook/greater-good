5af07dfca686b9742aed91058e902012eb847abb
Zach Wyman <zachwyman@zachs-mbp.attlocal.net>
2017-11-10 10:29:20 -0500

    calendar backup

--- resources/views/calendarUser.blade.php
+++ resources/views/calendarUser.blade.php
@@ -5,76 +5,10 @@
 <head>
     <title>Mill Village Farms </title>
     <meta charset="utf-8" />
-    <style>
-* {box-sizing: border-box;}
-ul {list-style-type: none;}
-body {font-family: Verdana, sans-serif;}
-
-.month {
-    padding: 70px 25px;
-    width: 100%;
-    background: #1abc9c;
-    text-align: center;
-}
-
-.month ul {
-    margin: 0;
-    padding: 0;
-}
-
-.month ul li {
-    color: white;
-    font-size: 20px;
-    text-transform: uppercase;
-    letter-spacing: 3px;
-}
-
-.month .prev {
-    float: left;
-    padding-top: 10px;
-}
-
-.month .next {
-    float: right;
-    padding-top: 10px;
-}
-
-.weekdays {
-    margin: 0;
-    padding: 10px 0;
-    background-color: #ddd;
-}
-
-.weekdays li {
-    display: inline-block;
-    width: 13.6%;
-    color: #666;
-    text-align: center;
-}
-
-.days {
-    padding: 10px 0;
-    background: #eee;
-    margin: 0;
-}
-
-.days li {
-    list-style-type: none;
-    display: inline-block;
-    width: 13.6%;
-    text-align: center;
-    margin-bottom: 5px;
-    font-size:12px;
-    color: #777;
-}
-
-.days li .active {
-    padding: 5px;
-    background: #1abc9c;
-    color: white !important
-}
-
-</style>
+    <link rel="stylesheet" href="StyleSheet1.css">
+    <link rel="stylesheet" href="bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
 </head>
 
 <body>
@@ -93,62 +27,7 @@ body {font-family: Verdana, sans-serif;}
             <button id="make"> Make </button>
             <button id="students"> Students </button>
         </div>
-        <div class="month">
-          <ul>
-            <li class="prev">&#10094;</li>
-            <li class="next">&#10095;</li>
-            <li>
-              August<br>
-              <span style="font-size:18px">2017</span>
-            </li>
-          </ul>
-        </div>
-
-        <ul class="weekdays">
-          <li>Mo</li>
-          <li>Tu</li>
-          <li>We</li>
-          <li>Th</li>
-          <li>Fr</li>
-          <li>Sa</li>
-          <li>Su</li>
-        </ul>
-
-        <ul class="days">
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-          <li>6</li>
-          <li>7</li>
-          <li>8</li>
-          <li>9</li>
-          <li><span class="active">10</span></li>
-          <li>11</li>
-          <li>12</li>
-          <li>13</li>
-          <li>14</li>
-          <li>15</li>
-          <li>16</li>
-          <li>17</li>
-          <li>18</li>
-          <li>19</li>
-          <li>20</li>
-          <li>21</li>
-          <li>22</li>
-          <li>23</li>
-          <li>24</li>
-          <li>25</li>
-          <li>26</li>
-          <li>27</li>
-          <li>28</li>
-          <li>29</li>
-          <li>30</li>
-          <li>31</li>
-        </ul>
-
-        <!-- <h3> February </h3>
+        <h3> February </h3>
         <table style = "margin-top: 20px;">
           <tr>
             <td style="border-style: solid; padding-left: 150px; padding-bottom: 100px;">1 </td>
@@ -186,7 +65,7 @@ body {font-family: Verdana, sans-serif;}
             <td style="border-style: solid; padding-left: 150px; padding-bottom: 100px;">27 </td>
             <td style="border-style: solid; padding-left: 150px; padding-bottom: 100px;">28 </td>
           </tr>
-        </table> -->
+        </table>
 
 
         <!-- <div class="calendar">
